3e06fcdf630c7e344f8ed449c5710076a5abecbd
Shraddha-sakhare <shraddhasakhare111@gmail.com>
2024-09-27 02:21:29 +0530

    Add files via upload

---
+++ ADS/Assignments /Assignment -1/LeapYr.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class LeapYr{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter  year: ");
+        int year = sc.nextInt();
+
+        if (isLeapYear(year)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                return year % 400 == 0;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
