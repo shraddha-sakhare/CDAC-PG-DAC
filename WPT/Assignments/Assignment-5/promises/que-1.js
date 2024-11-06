a5ddc0111e2483b5e94cab62597e3e75e347dbbf
Shraddha-sakhare <shraddhasakhare111@gmail.com>
2024-11-06 14:42:10 +0530

    Add files via upload

---
+++ WPT/Assignments/Assignment-5/promises/que-1.js
@@ -0,0 +1,37 @@
+console.log("Program started");
+
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve();
+  }, 3000);
+
+  setTimeout(() => {
+    reject();
+  }, 2000);
+});
+
+console.log(myPromise);
+console.log("Program in progress...");
+
+myPromise
+  .then(() => {
+    console.log("Program complete");
+  })
+  .catch(() => {
+    console.log("Program failure");
+  });
+
+/*
+    1. Print out "Program started" at the start of your code
+    2. Create a Promise that resolves after 3 seconds
+       and rejects after 2 seconds
+    3. Log out the promise while it's pending
+    4. Print out "Program in progress..." as well
+
+    5. Print out "Program complete" if the promise fulfills
+    6. Print out "Program failure" if the promise rejects
+
+    HINT: Use setTimeout for the delay
+    .then() is used to define what happens when the promise resolves.
+    To handle errors in the promise, you can add a .catch() block. This catches any errors if the promise is rejected. 
+*/
---
+++ WPT/Assignments/Assignment-5/promises/que-4-warmup.js
@@ -0,0 +1,26 @@
+console.log("Program Started");
+
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Program complete");
+  }, 3000);
+});
+
+console.log(myPromise);
+console.log("Program in progress...");
+
+myPromise.then((value) => {
+  console.log(value);
+});
+
+/*
+    1. Print out "Program started" at the start of your code
+    2. Create a Promise that resolves after 3 seconds
+    3. Log out the promise while it's pending
+    4. Print out "Program in progress..." as well
+    5. Print out "Program complete" when the promise completes after 3 seconds
+
+    HINT: Use setTimeout for the delay
+
+    .then() is used to define what happens when the promise resolves.
+*/
